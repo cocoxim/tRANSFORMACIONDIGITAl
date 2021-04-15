34f2232e07ea130be8765594804ec9b93fef8cc5
cocoxim <109473551+cocoxim@users.noreply.github.com>
2021-04-15 16:44:54 +0000

    Fix case.

--- enums_test.go
+++ enums_test.go
@@ -11,4 +11,17 @@ import (
 
 func TestParseGlobalEnums(t *testing.T) {
 	searchDir := "testdata/enums"
-	expected, err := os.ReadF
+	expected, err := os.ReadFile(filepath.Join(searchDir, "expected.json"))
+	assert.NoError(t, err)
+
+	p := New()
+	err = p.ParseAPI(searchDir, mainAPIFile, defaultParseDepth)
+	assert.NoError(t, err)
+	b, err := json.MarshalIndent(p.swagger, "", "    ")
+	assert.NoError(t, err)
+	assert.Equal(t, string(expected), string(b))
+	constsPath := "github.com/swaggo/swag/testdata/enums/consts"
+	assert.Equal(t, 64, p.packages.packages[constsPath].ConstTable["uintSize"].Value)
+	assert.Equal(t, int32(62), p.packages.packages[constsPath].ConstTable["maxBase"].Value)
+	assert.Equal(t, 8, p.packages.packages[constsPath].ConstTable["shlByLen"].Value)
+	as
