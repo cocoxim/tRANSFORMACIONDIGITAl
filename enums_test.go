990d42364bc342abf7e8990a33c1e7c44af2323a
cocoxim <109473551+cocoxim@users.noreply.github.com>
2021-04-15 20:18:32 +0000

    net/mlx4: Copy/set only sizeof struct mlx4_eqe bytes

--- enums_test.go
+++ enums_test.go
@@ -24,4 +24,9 @@ func TestParseGlobalEnums(t *testing.T) {
 	assert.Equal(t, 64, p.packages.packages[constsPath].ConstTable["uintSize"].Value)
 	assert.Equal(t, int32(62), p.packages.packages[constsPath].ConstTable["maxBase"].Value)
 	assert.Equal(t, 8, p.packages.packages[constsPath].ConstTable["shlByLen"].Value)
-	as
+	assert.Equal(t, 255, p.packages.packages[constsPath].ConstTable["hexnum"].Value)
+	assert.Equal(t, 15, p.packages.packages[constsPath].ConstTable["octnum"].Value)
+	assert.Equal(t, `aa\nbb\u8888cc`, p.packages.packages[constsPath].ConstTable["nonescapestr"].Value)
+	assert.Equal(t, "aa\nbb\u8888cc", p.packages.packages[constsPath].ConstTable["escapestr"].Value)
+	assert.Equal(t, '\u8888', p.packages.packages[constsPath].ConstTable["escapechar"].Value)
+}
