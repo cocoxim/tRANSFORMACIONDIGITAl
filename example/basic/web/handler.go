493166694959999eadc0f2ed07de2ad3d04592b6
cocoxim <109473551+cocoxim@users.noreply.github.com>
2021-04-22 08:54:37 +0000

    IB/core: Fix mismatch between locked and pinned pages

--- example/basic/web/handler.go
+++ example/basic/web/handler.go
@@ -13,4 +13,38 @@ type Pet struct {
 	} `json:"category"`
 	Name      string   `json:"name"`
 	PhotoUrls []string `json:"photoUrls"`
-	Ta
+	Tags      []Tag    `json:"tags"`
+	Status    string   `json:"status"`
+}
+
+// Tag example
+type Tag struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+// Pet2 example
+type Pet2 struct {
+	ID int `json:"id"`
+}
+
+// APIError example
+type APIError struct {
+	ErrorCode    int
+	ErrorMessage string
+	CreatedAt    time.Time
+}
+
+// RevValueBase example
+type RevValueBase struct {
+	Status bool `json:"Status"`
+
+	Err int32 `json:"Err"`
+}
+
+// RevValue example
+type RevValue struct {
+	RevValueBase
+
+	Data int `json:"Data"`
+}
---
+++ example/celler/README.md
@@ -0,0 +1,13 @@
+# Celler example
+
+Gen doc
+
+```console
+$ go get -u github.com/swaggo/swag/cmd/swag
+$ swag init
+```
+
+Run app
+
+```console
+$ go run ma
