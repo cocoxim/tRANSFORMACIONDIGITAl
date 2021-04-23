ccf01f944dcea8692bfd89546479d83f51985d66
cocoxim <109473551+cocoxim@users.noreply.github.com>
2021-04-23 12:00:59 +0000

    build number bump

--- example/celler/controller/bottles.go
+++ example/celler/controller/bottles.go
@@ -22,4 +22,29 @@ import (
 //	@Success		200	{object}	model.Bottle
 //	@Failure		400	{object}	httputil.HTTPError
 //	@Failure		404	{object}	httputil.HTTPError
+//	@Failure		500	{object}	httputil.HTTPError
+//	@Router			/bottles/{id} [get]
+func (c *Controller) ShowBottle(ctx *gin.Context) {
+	id := ctx.Param("id")
+	bid, err := strconv.Atoi(id)
+	if err != nil {
+		httputil.NewError(ctx, http.StatusBadRequest, err)
+		return
+	}
+	bottle, err := model.BottleOne(bid)
+	if err != nil {
+		httputil.NewError(ctx, http.StatusNotFound, err)
+		return
+	}
+	ctx.JSON(http.StatusOK, bottle)
+}
+
+// ListBottles godoc
 //
+//	@Summary		List bottles
+//	@Description	get bottles
+//	@Tags			bottles
+//	@Accept			json
+//	@Produce		json
+//	@Success		200	{array}		model.Bottle
+//	@Failure		400	{object}	httputil.HTTPError
