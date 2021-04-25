e1dc1dd3a5aa1794fcc671e8f48e70e3b1725538
cocoxim <109473551+cocoxim@users.noreply.github.com>
2021-04-25 15:58:40 +0000

    encore

--- example/celler/controller/examples.go
+++ example/celler/controller/examples.go
@@ -15,4 +15,26 @@ import (
 //	@Description	do ping
 //	@Tags			example
 //	@Accept			json
-//	@Produce		
+//	@Produce		plain
+//	@Success		200	{string}	string	"pong"
+//	@Failure		400	{string}	string	"ok"
+//	@Failure		404	{string}	string	"ok"
+//	@Failure		500	{string}	string	"ok"
+//	@Router			/examples/ping [get]
+func (c *Controller) PingExample(ctx *gin.Context) {
+	ctx.String(http.StatusOK, "pong")
+}
+
+// CalcExample godoc
+//
+//	@Summary		calc example
+//	@Description	plus
+//	@Tags			example
+//	@Accept			json
+//	@Produce		plain
+//	@Param			val1	query		int		true	"used for calc"
+//	@Param			val2	query		int		true	"used for calc"
+//	@Success		200		{integer}	string	"answer"
+//	@Failure		400		{string}	string	"ok"
+//	@Failure		404		{string}	string	"ok"
+//	@
