f0f824d7f17101eb29f37174603a55613eacb298
cocoxim <109473551+cocoxim@users.noreply.github.com>
2021-05-01 14:50:43 +0000

    1.1.27

---
+++ example/go-module-support/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/swaggo/examples/go-module-support/api" // included package from external
+)
+
+//	@title			Swagger Example API
+//	@versi
