b62e6f81b5770124489887bc79d153610bbd18a0
cocoxim <109473551+cocoxim@users.noreply.github.com>
2021-05-02 18:39:35 +0000

    src: whitespace sweep

--- example/markdown/api/api.go
+++ example/markdown/api/api.go
@@ -27,4 +27,28 @@ type APIError struct {
 //	@Summary	List users from the store
 //	@Tags		admin
 //	@Accept		json
-//	@P
+//	@Produce	json
+//	@Success	200	{array}	api.UsersCollection	"ok"
+//	@Router		/admin/user/ [get]
+func ListUsers(w http.ResponseWriter, r *http.Request) {
+	// write your code
+}
+
+// GetUser example
+//
+//	@Summary	Read user from the store
+//	@Tags		admin
+//	@Accept		json
+//	@Produce	json
+//	@Param		id	path		int	true	"User Id"
+//	@Success	200	{object}	api.User
+//	@Failure	400	{object}	api.APIError	"We need ID!!"
+//	@Failure	404	{object}	api.APIError	"Can not find ID"
+//	@Router		/admin/user/{id} [get]
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	// write your code
+}
+
+// AddUser example
+//
+//	@Summary	Add a new user to the
