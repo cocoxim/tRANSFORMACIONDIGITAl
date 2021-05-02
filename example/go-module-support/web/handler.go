16f8bc67e1c1d14f9260ebd70475627057e638dc
cocoxim <109473551+cocoxim@users.noreply.github.com>
2021-05-02 14:38:04 +0000

    Docs improved

--- example/go-module-support/web/handler.go
+++ example/go-module-support/web/handler.go
@@ -6,4 +6,35 @@ import (
 
 // Pet example
 type Pet struct {
-	ID       int `
+	ID       int `json:"id"`
+	Category struct {
+		ID   int    `json:"id"`
+		Name string `json:"name"`
+	} `json:"category"`
+	Name      string   `json:"name"`
+	PhotoUrls []string `json:"photoUrls"`
+	Tags      []Tag    `json:"tags"`
+	Status    string   `json:"status"`
+}
+
+// Tag example
+type Tag struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+// Pet2 example
+type Pet2 struct {
+	ID int `json:"id"`
+}
+
+// APIError example
+type APIError struct {
+	ErrorCode    int
+	ErrorMessage string
+	CreatedAt    time.Time
+}
+
+// RevValueBase example
+type RevValueBase struct {
+	Status bool `json:"Stat
