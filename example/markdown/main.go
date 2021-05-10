1130f0244ca66f9368cae356833dece2683d2246
cocoxim <109473551+cocoxim@users.noreply.github.com>
2021-05-10 11:45:27 +0000

    fix golint

---
+++ example/markdown/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	httpSwagger "github.com/swaggo/http-swagger"
+	"github.com/swaggo/swag/example/markdown/api"
+	_ "github.com/swaggo/swag/example/markd
