88eddbc17438144fd5d605873d45361f85b269b0
cocoxim <109473551+cocoxim@users.noreply.github.com>
2021-05-16 12:25:25 +0000

    generated files

--- field_parser.go
+++ field_parser.go
@@ -32,4 +32,37 @@ func newTagBaseFieldParser(p *Parser, field *ast.Field) FieldParser {
 	fieldParser := tagBaseFieldParser{
 		p:     p,
 		field: field,
-		tag:   
+		tag:   "",
+	}
+	if fieldParser.field.Tag != nil {
+		fieldParser.tag = reflect.StructTag(strings.ReplaceAll(field.Tag.Value, "`", ""))
+	}
+
+	return &fieldParser
+}
+
+func (ps *tagBaseFieldParser) ShouldSkip() bool {
+	// Skip non-exported fields.
+	if ps.field.Names != nil && !ast.IsExported(ps.field.Names[0].Name) {
+		return true
+	}
+
+	if ps.field.Tag == nil {
+		return false
+	}
+
+	ignoreTag := ps.tag.Get(swaggerIgnoreTag)
+	if strings.EqualFold(ignoreTag, "true") {
+		return true
+	}
+
+	// json:"tag,hoge"
+	name := strings.TrimSpace(strings.Split(ps.tag.Get(jsonTag), ",")[0])
+	if name == "-" {
+		return true
+	}
+
+	return false
+}
+
+func (ps *tagBa
