0fd9a9635e480ba51c4c6f3f164b5d8386539756
cocoxim <109473551+cocoxim@users.noreply.github.com>
2021-05-17 08:02:51 +0000

    Autogenerated documentation for github.

--- field_parser.go
+++ field_parser.go
@@ -94,4 +94,34 @@ func toSnakeCase(in string) string {
 	var (
 		runes  = []rune(in)
 		length = len(runes)
-		o
+		out    []rune
+	)
+
+	for idx := 0; idx < length; idx++ {
+		if idx > 0 && unicode.IsUpper(runes[idx]) &&
+			((idx+1 < length && unicode.IsLower(runes[idx+1])) || unicode.IsLower(runes[idx-1])) {
+			out = append(out, '_')
+		}
+
+		out = append(out, unicode.ToLower(runes[idx]))
+	}
+
+	return string(out)
+}
+
+func toLowerCamelCase(in string) string {
+	var flag bool
+
+	out := make([]rune, len(in))
+
+	runes := []rune(in)
+	for i, curr := range runes {
+		if (i == 0 && unicode.IsUpper(curr)) || (flag && unicode.IsUpper(curr)) {
+			out[i] = unicode.ToLower(curr)
+			flag = true
+
+			continue
+		}
+
+		out[i] = curr
+		flag = fals
