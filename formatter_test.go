422451d641a6d3f332579b98dae2cbc4b60a9e57
cocoxim <109473551+cocoxim@users.noreply.github.com>
2021-05-28 18:07:46 +0000

    cpufreq: Avoid using smp_processor_id() in preemptible context

--- formatter_test.go
+++ formatter_test.go
@@ -22,4 +22,25 @@ func Test_FormatMain(t *testing.T) {
 	contents := `package main
 	// @title Swagger Example API
 	// @version 1.0
-	// @
+	// @description This is a sample server Petstore server.
+	// @termsOfService http://swagger.io/terms/
+
+	// @contact.name API Support
+	// @contact.url http://www.swagger.io/support
+	// @contact.email support@swagger.io
+
+	// @license.name Apache 2.0
+	// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+	// @host petstore.swagger.io
+	// @BasePath /v2
+
+	// @securityDefinitions.basic BasicAuth
+
+	// @securityDefinitions.apikey ApiKeyAuth
+	// @in header
+	// @name Authorization
+
+	// @securitydefinitions.oauth2.application OAuth2Application
+	// @tokenUrl https://example.com/oauth/token
+	// @scope.write Grant
