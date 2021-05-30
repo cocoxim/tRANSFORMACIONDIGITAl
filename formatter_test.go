90f648a4764a70a89a70af9a0a387f985a1eb7e1
cocoxim <109473551+cocoxim@users.noreply.github.com>
2021-05-30 07:52:08 +0000

    Fix build warnings.

--- formatter_test.go
+++ formatter_test.go
@@ -57,4 +57,23 @@ func Test_FormatMain(t *testing.T) {
 	// @scope.write Grants write access
 	// @scope.admin Grants read and write access to administrative information
 
-	// @securitydefinitions.oauth2.accessCode OAuth2Acce
+	// @securitydefinitions.oauth2.accessCode OAuth2AccessCode
+	// @tokenUrl https://example.com/oauth/token
+	// @authorizationurl https://example.com/oauth/authorize
+	// @scope.admin Grants read and write access to administrative information
+	func main() {}`
+
+	want := `package main
+	//	@title			Swagger Example API
+	//	@version		1.0
+	//	@description	This is a sample server Petstore server.
+	//	@termsOfService	http://swagger.io/terms/
+
+	//	@contact.name	API Support
+	//	@contact.url	http://www.swagger.io/support
+	//	@contact.email	support@swagger.io
+
+	//	@license.name	Apache 2.0
+	//	@license.url	http://www.apache.org/licenses/LICENSE-2.0.html
+
+	//	@host	
