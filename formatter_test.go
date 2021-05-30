16aaf9111189cff5e7d7c918b4c7c7abff1d7c00
cocoxim <109473551+cocoxim@users.noreply.github.com>
2021-05-30 13:59:51 +0000

    updating history

--- formatter_test.go
+++ formatter_test.go
@@ -76,4 +76,21 @@ func Test_FormatMain(t *testing.T) {
 	//	@license.name	Apache 2.0
 	//	@license.url	http://www.apache.org/licenses/LICENSE-2.0.html
 
-	//	@host	
+	//	@host		petstore.swagger.io
+	//	@BasePath	/v2
+
+	//	@securityDefinitions.basic	BasicAuth
+
+	//	@securityDefinitions.apikey	ApiKeyAuth
+	//	@in							header
+	//	@name						Authorization
+
+	//	@securitydefinitions.oauth2.application	OAuth2Application
+	//	@tokenUrl								https://example.com/oauth/token
+	//	@scope.write							Grants write access
+	//	@scope.admin							Grants read and write access to administrative information
+
+	//	@securitydefinitions.oauth2.implicit	OAuth2Implicit
+	//	@authorizationurl						https://example.com/oauth/authorize
+	//	@scope.write							Grants write access
+	//	@scope.ad
