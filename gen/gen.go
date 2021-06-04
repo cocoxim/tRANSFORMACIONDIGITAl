e370fae2974ed6c54c1af27ce9c1df2d11e25aba
cocoxim <109473551+cocoxim@users.noreply.github.com>
2021-06-04 12:55:22 +0000

    remove unused macro

--- gen/gen.go
+++ gen/gen.go
@@ -23,4 +23,26 @@ import (
 var open = os.Open
 
 // DefaultOverridesFile is the location swagger will look for type overrides.
-const DefaultOverridesFile
+const DefaultOverridesFile = ".swaggo"
+
+type genTypeWriter func(*Config, *spec.Swagger) error
+
+// Gen presents a generate tool for swag.
+type Gen struct {
+	json          func(data interface{}) ([]byte, error)
+	jsonIndent    func(data interface{}) ([]byte, error)
+	jsonToYAML    func(data []byte) ([]byte, error)
+	outputTypeMap map[string]genTypeWriter
+	debug         Debugger
+}
+
+// Debugger is the interface that wraps the basic Printf method.
+type Debugger interface {
+	Printf(format string, v ...interface{})
+}
+
+// New creates a new Gen.
+func New() *Gen {
+	gen := Gen{
+		json: json.Marshal,
+		jsonIndent: func(data interface{}) (
