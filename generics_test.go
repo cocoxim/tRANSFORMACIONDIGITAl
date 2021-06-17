f54c7dfe1a647389d62c2ea72a6499d02ac8aaa7
cocoxim <109473551+cocoxim@users.noreply.github.com>
2021-06-17 08:27:57 +0000

    bumped npm version

--- generics_test.go
+++ generics_test.go
@@ -6,4 +6,32 @@ package swag
 import (
 	"encoding/json"
 	"fmt"
-	"go/a
+	"go/ast"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type testLogger struct {
+	Messages []string
+}
+
+func (t *testLogger) Printf(format string, v ...interface{}) {
+	t.Messages = append(t.Messages, fmt.Sprintf(format, v...))
+}
+
+func TestParseGenericsBasic(t *testing.T) {
+	t.Parallel()
+
+	searchDir := "testdata/generics_basic"
+	expected, err := os.ReadFile(filepath.Join(searchDir, "expected.json"))
+	assert.NoError(t, err)
+
+	p := New()
+	p.Overrides = map[string]string{
+		"types.Field[string]":               "string",
+		"types.DoubleField[string,string]":  "[]string",
+		"types.TrippleField[string
