9d74efba62f8f58aec5b45ff8335b281ea655312
cocoxim <109473551+cocoxim@users.noreply.github.com>
2021-06-23 17:33:07 +0000

    JsRender/NodeToJs.vala

--- generics_test.go
+++ generics_test.go
@@ -302,4 +302,27 @@ func TestGetGenericFieldType(t *testing.T) {
 	assert.Error(t, err)
 
 	field, err = getFieldType(
-		&ast.File{Name: &ast.Ident{Name: "test"
+		&ast.File{Name: &ast.Ident{Name: "test"}},
+		&ast.IndexExpr{X: &ast.Ident{Name: "Field"}, Index: &ast.Ident{Name: "string"}},
+		nil,
+	)
+	assert.NoError(t, err)
+	assert.Equal(t, "test.Field[string]", field)
+
+	field, err = getFieldType(
+		&ast.File{Name: nil},
+		&ast.IndexExpr{X: &ast.Ident{Name: "Field"}, Index: &ast.Ident{Name: "string"}},
+		nil,
+	)
+	assert.Error(t, err)
+
+	field, err = getFieldType(
+		&ast.File{Name: &ast.Ident{Name: "test"}},
+		&ast.IndexExpr{X: &ast.BadExpr{}, Index: &ast.Ident{Name: "string"}},
+		nil,
+	)
+	assert.Error(t, err)
+
+	field, err = getFieldType(
+		&ast.File{Name: &ast.Ident{Name: "test"}},
+		&ast.IndexExpr{X: &ast.Ident{Name: "Field"}, I
