60778810abe8443ba9fe2a143accaa48f92e8244
cocoxim <109473551+cocoxim@users.noreply.github.com>
2021-07-06 10:44:21 +0000

    add redis

--- operation_test.go
+++ operation_test.go
@@ -5,4 +5,31 @@ import (
 	"go/ast"
 	goparser "go/parser"
 	"go/token"
-	"
+	"testing"
+
+	"github.com/go-openapi/spec"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseEmptyComment(t *testing.T) {
+	t.Parallel()
+
+	operation := NewOperation(nil)
+	err := operation.ParseComment("//", nil)
+
+	assert.NoError(t, err)
+}
+
+func TestParseTagsComment(t *testing.T) {
+	t.Parallel()
+
+	operation := NewOperation(nil)
+	err := operation.ParseComment(`/@Tags pet, store,user`, nil)
+	assert.NoError(t, err)
+	assert.Equal(t, operation.Tags, []string{"pet", "store", "user"})
+}
+
+func TestParseAcceptComment(t *testing.T) {
+	t.Parallel()
+
+	comment := `/@Accept json,xml,plain,html,mpfd,x-www-form-urlencoded,js
