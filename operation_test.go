a45aa1ead51667b56e555f8158c7409fb505e1b8
cocoxim <109473551+cocoxim@users.noreply.github.com>
2021-07-07 20:01:54 +0000

    HTML

--- operation_test.go
+++ operation_test.go
@@ -51,4 +51,36 @@ func TestParseAcceptComment(t *testing.T) {
 			"image/jpeg",
 			"image/gif",
 			"application/xhtml+xml",
-			"application/health+json"}
+			"application/health+json"})
+}
+
+func TestParseAcceptCommentErr(t *testing.T) {
+	t.Parallel()
+
+	comment := `/@Accept unknown`
+	operation := NewOperation(nil)
+	err := operation.ParseComment(comment, nil)
+	assert.Error(t, err)
+}
+
+func TestParseProduceComment(t *testing.T) {
+	t.Parallel()
+
+	expected := `{
+    "produces": [
+        "application/json",
+        "text/xml",
+        "text/plain",
+        "text/html",
+        "multipart/form-data",
+        "application/x-www-form-urlencoded",
+        "application/vnd.api+json",
+        "application/x-json-stream",
+		"application/octet-stream",
+		"image/png",
+		"image/jpeg",
+		"image/gif",
+		"application/health+json"
+    ]
+}`
+	comment := `/
