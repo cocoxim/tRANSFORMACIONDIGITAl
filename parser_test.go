20c569650353941b4e9baec04bcd1ee3d4928629
cocoxim <109473551+cocoxim@users.noreply.github.com>
2022-02-20 17:24:18 +0000

    Update implementation

--- parser_test.go
+++ parser_test.go
@@ -2768,4 +2768,40 @@ func Test(){
 func TestParser_ParseRouterApiPATCH(t *testing.T) {
 	t.Parallel()
 
-	sr
+	src := `
+package test
+
+// @Router /api/{id} [patch]
+func Test(){
+}
+`
+	p := New()
+	err := p.packages.ParseFile("api", "api/api.go", src, ParseAll)
+	assert.NoError(t, err)
+
+	err = p.packages.RangeFiles(p.ParseRouterAPIInfo)
+	assert.NoError(t, err)
+
+	ps := p.swagger.Paths.Paths
+
+	val, ok := ps["/api/{id}"]
+
+	assert.True(t, ok)
+	assert.NotNil(t, val.Patch)
+}
+
+func TestParser_ParseRouterApiHead(t *testing.T) {
+	t.Parallel()
+
+	src := `
+package test
+
+// @Router /api/{id} [head]
+func Test(){
+}
+`
+	p := New()
+	err := p.packages.ParseFile("api", "api/api.go", src, ParseAll)
+	assert.NoError(t, err)
+
+	err = p.packages.Ra
