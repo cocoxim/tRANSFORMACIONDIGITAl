b800b3b724b180868e43c43ac3a7e5c9cc1b1c8e
cocoxim <109473551+cocoxim@users.noreply.github.com>
2022-04-10 15:45:17 +0000

    New translations django.po (German)

---
+++ testdata/alias_import/main.go
@@ -0,0 +1,15 @@
+package alias_import
+
+import (
+	"net/http"
+
+	"github.com/swaggo/swag/testdata/alias_import/api"
+)
+
+// @title Swagger Example API
+// @version 1.0
+// @description This is a sample server Petstore server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagge
