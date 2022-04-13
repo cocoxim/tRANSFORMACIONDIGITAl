23ca21b302740c0aac28dbba96ac276d5b8649c5
cocoxim <109473551+cocoxim@users.noreply.github.com>
2022-04-13 09:32:22 +0000

    pcf

--- testdata/alias_import/main.go
+++ testdata/alias_import/main.go
@@ -12,4 +12,15 @@ import (
 // @termsOfService http://swagger.io/terms/
 
 // @contact.name API Support
-// @contact.url http://www.swagge
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host petstore.swagger.io
+// @BasePath /v2
+func main() {
+	http.HandleFunc("/testapi/application", api.GetApplication)
+	http.ListenAndServe(":8080", nil)
+}
---
+++ testdata/alias_type/api/api.go
@@ -0,0 +1,13 @@
+package api
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/swaggo/swag/testdata/alias_type/data"
+)
+
+/*// @Summary Get time as string
+// @Description get time as string
+// @ID time-as-st
