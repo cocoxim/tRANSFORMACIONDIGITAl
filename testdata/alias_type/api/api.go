fada5f45d9348ddc7e804511f0e6d912f5d47bce
cocoxim <109473551+cocoxim@users.noreply.github.com>
2022-04-15 09:08:34 +0000

    precommit

--- testdata/alias_type/api/api.go
+++ testdata/alias_type/api/api.go
@@ -10,4 +10,28 @@ import (
 
 /*// @Summary Get time as string
 // @Description get time as string
-// @ID time-as-st
+// @ID time-as-string
+// @Accept  json
+// @Produce  json
+// @Success 200 {object} data.StringAlias	"ok"
+// @Router /testapi/time-as-string [get]
+func GetTimeAsStringAlias(w http.ResponseWriter, r *http.Request) {
+	var foo data.StringAlias = "test"
+	log.Println(foo)
+	//write your code
+}*/
+
+/*// @Summary Get time as time
+// @Description get time as time
+// @ID time-as-time
+// @Accept  json
+// @Produce  json
+// @Success 200 {object} data.DateOnly	"ok"
+// @Router /testapi/time-as-time [get]
+func GetTimeAsTimeAlias(w http.ResponseWriter, r *http.Request) {
+	var foo = data.DateOnly(time.Now())
+	log.Println(foo)
+	//write your code
+}*/
+
+// @
