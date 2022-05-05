bc275ad5f82d70413e7d8df9f76cc0b32c53a79c
cocoxim <109473551+cocoxim@users.noreply.github.com>
2022-05-05 07:55:03 +0000

    language update

--- testdata/error/api/api.go
+++ testdata/error/api/api.go
@@ -9,4 +9,15 @@ import (
 
 // Upload do something
 // @Summary Upload file
-// @Description Upl
+// @Description Upload file
+// @ID file.upload
+// @Accept  multipart/form-data
+// @Produce  json
+// @Param   file formData file true  "this is a test file"
+// @Success 200 {string} string "ok"
+// @Failure 400 {object} web.CrossErrors "Abort !!"
+// @Router /file/upload [post]
+func Upload(w http.ResponseWriter, r *http.Request) {
+	//write your code
+	_ = Errors{}
+}
