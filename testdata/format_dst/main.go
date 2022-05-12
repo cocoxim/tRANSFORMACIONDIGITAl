93cc6a692eebc71a12d8398160c4b6f604be2a90
cocoxim <109473551+cocoxim@users.noreply.github.com>
2022-05-12 19:03:25 +0000

    ndb - merge 70 to 71

--- testdata/format_dst/main.go
+++ testdata/format_dst/main.go
@@ -35,4 +35,15 @@ import (
 // @securitydefinitions.oauth2.implicit  OAuth2Implicit
 // @authorizationurl                     https://example.com/oauth/authorize
 // @scope.write                          Grants write access
-// @scope.admin                          Grants read and write access to a
+// @scope.admin                          Grants read and write access to administrative information
+
+// @securitydefinitions.oauth2.password  OAuth2Password
+// @tokenUrl                             https://example.com/oauth/token
+// @scope.read                           Grants read access
+// @scope.write                          Grants write access
+// @scope.admin                          Grants read and write access to administrative information
+
+// @securitydefinitions.oauth2.accessCode  OAuth2AccessCode
+// @tokenUrl                               https://example.com/oauth/token
+// @authorizationurl                       https://example.com/oauth/authorize
+// @scope.admin                            Grants read 
