718f76653ae52a53036368dd061d137bc1f2b374
cocoxim <109473551+cocoxim@users.noreply.github.com>
2022-05-15 22:00:19 +0000

    permission

--- testdata/format_src/api/api.go
+++ testdata/format_src/api/api.go
@@ -8,4 +8,20 @@ import (
 
 // @Summary Add a new pet to the store
 // @Description get string by ID
-// @ID get-s
+// @ID get-string-by-int
+// @Accept  json
+// @Produce  json
+// @Param   some_id      path   int     true  "Some ID" Format(int64)
+// @Param   some_id      body web.Pet true  "Some ID"
+// @Success 200 {string} string	"ok"
+// @Failure 400 {object} web.APIError "We need ID!!"
+// @Failure 404 {object} web.APIError "Can not find ID"
+// @Router /testapi/get-string-by-int/{some_id} [get]
+func GetStringByInt(w http.ResponseWriter, r *http.Request) {
+	//write your code
+}
+
+// @Description get struct array by ID
+// @ID get-struct-array-by-string
+// @Accept  json
+// @Produce  jso
