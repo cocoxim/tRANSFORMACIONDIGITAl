dd2f82598d1e084739cca16521c0c6bc380fe75e
cocoxim <109473551+cocoxim@users.noreply.github.com>
2022-05-16 14:05:37 +0000

    delete some files

--- testdata/format_src/api/api.go
+++ testdata/format_src/api/api.go
@@ -24,4 +24,17 @@ func GetStringByInt(w http.ResponseWriter, r *http.Request) {
 // @Description get struct array by ID
 // @ID get-struct-array-by-string
 // @Accept  json
-// @Produce  jso
+// @Produce  json
+// @Param some_id path string true "Some ID"
+// @Param category query int true "Category" Enums(1, 2, 3)
+// @Param offset query int true "Offset" Minimum(0) default(0)
+// @Param limit query int true "Limit" Maximum(50) default(10)
+// @Param q query string true "q" Minlength(1) Maxlength(50) default("")
+// @Success 200 {string} string	"ok"
+// @Failure 400 {object} web.APIError "We need ID!!"
+// @Failure 404 {object} web.APIError "Can not find ID"
+// @Security ApiKeyAuth
+// @Security BasicAuth
+// @Security OAuth2Application[write]
+// @Security OAuth2Implicit[read, admin]
+// @Security OAuth2Acces
