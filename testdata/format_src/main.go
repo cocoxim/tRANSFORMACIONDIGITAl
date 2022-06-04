13cc2e8e2b19162e344f7163232b67fa6e2e06ac
cocoxim <109473551+cocoxim@users.noreply.github.com>
2022-06-04 14:19:49 +0000

    Updated ignore list.

--- testdata/format_src/main.go
+++ testdata/format_src/main.go
@@ -38,4 +38,17 @@ import (
 // @scope.admin Grants read and write access to administrative information
 
 // @securitydefinitions.oauth2.password OAuth2Password
-// @tokenUrl https://example.com/oauth/toke
+// @tokenUrl https://example.com/oauth/token
+// @scope.read Grants read access
+// @scope.write Grants write access
+// @scope.admin Grants read and write access to administrative information
+
+// @securitydefinitions.oauth2.accessCode OAuth2AccessCode
+// @tokenUrl https://example.com/oauth/token
+// @authorizationurl https://example.com/oauth/authorize
+// @scope.admin Grants read and write access to administrative information
+func main() {
+	http.HandleFunc("/testapi/get-string-by-int/", api.GetStringByInt)
+	http.HandleFunc("/testapi/get-struct-array-by-string/", api.GetStructArrayByString)
+	http.HandleFunc("/testapi/upload", api.Upload)
+	http.List
