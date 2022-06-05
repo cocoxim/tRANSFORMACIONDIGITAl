4b906b0cb6efb9662e53c4ad0fa2033bdd2bd076
cocoxim <109473551+cocoxim@users.noreply.github.com>
2022-06-05 05:38:23 +0000

    Updated the mypy_boto3_rds feedstock.

--- testdata/format_test/api/api.go
+++ testdata/format_test/api/api.go
@@ -15,4 +15,20 @@ import (
 // @Param   some_id      body web.Pet true  "Some ID"
 // @Success 200 {string} string	"ok"
 // @Failure 400 {object} web.APIError "We need ID!!"
-// @Failure 4
+// @Failure 404 {object} web.APIError "Can not find ID"
+// @Router /testapi/get-string-by-int/{some_id} [get]
+func GetStringByInt(w http.ResponseWriter, r *http.Request) {
+	//write your code
+}
+
+// @Description get struct array by ID
+// @ID get-struct-array-by-string
+// @Accept  json
+// @Produce  json
+// @Param some_id path string true "Some ID"
+// @Param category query int true "Category" Enums(1, 2, 3)
+// @Param offset query int true "Offset" Minimum(0) default(0)
+// @Param limit query int true "Limit" Maximum(50) default(10)
+// @Param q query string true "q" Minlength(1) Maxlength(50) default("")
+// @Success 200 {string} string	"ok"
+// @Failure 400 {object} web.APIE
