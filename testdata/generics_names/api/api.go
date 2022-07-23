7cf3ceb60e5ac10f62990705389ddceb4554e545
cocoxim <109473551+cocoxim@users.noreply.github.com>
2022-07-23 11:42:48 +0000

    Fixed headers.

--- testdata/generics_names/api/api.go
+++ testdata/generics_names/api/api.go
@@ -13,4 +13,19 @@ import (
 // @Produce  json
 // @Param   data        body   web.GenericBody[types.Post]    true  "Some ID"
 // @Success 200 {object} web.GenericResponse[types.Post]
-// @Success 222 {
+// @Success 222 {object} web.GenericResponseMulti[types.Post, types.Post]
+// @Failure 400 {object} web.APIError "We need ID!!"
+// @Failure 404 {object} web.APIError "Can not find ID"
+// @Router /posts/ [post]
+func GetPost(w http.ResponseWriter, r *http.Request) {
+	//write your code
+	_ = web.GenericResponse[types.Post]{}
+}
+
+// @Summary Add new pets to the store
+// @Description get string by ID
+// @Accept  json
+// @Produce  json
+// @Param   data	body	web.GenericBodyMulti[types.Post, types.Post]	true	"Some ID"
+// @Success 200 {object} web.GenericResponse[types.Post]
+// @Success 222 {object} web.Gene
