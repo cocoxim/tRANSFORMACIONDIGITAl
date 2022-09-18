23aaa003596c6004db67052260a2b15ab3868155
cocoxim <109473551+cocoxim@users.noreply.github.com>
2022-09-18 19:51:13 +0000

    Added a README.md

--- testdata/generics_package_alias/internal/api/api2.go
+++ testdata/generics_package_alias/internal/api/api2.go
@@ -1,1 +1,22 @@
-pac
+package api
+
+import (
+	myv1 "github.com/swaggo/swag/testdata/generics_package_alias/internal/path1/v1"
+	myv2 "github.com/swaggo/swag/testdata/generics_package_alias/internal/path2/v1"
+)
+
+// @Summary Create movie
+// @Description Create a new movie production
+// @Accept  json
+// @Produce  json
+// @Success 200 {object} myv2.ListResult[myv2.ProductDto] ""
+// @Router /api03 [post]
+func CreateMovie03() {
+	_ = myv2.ListResult[myv2.ProductDto]{}
+}
+
+// @Summary Create movie
+// @Description Create a new movie production
+// @Accept  json
+// @Produce  json
+// @Success 200 {object} myv2.RenamedListR
