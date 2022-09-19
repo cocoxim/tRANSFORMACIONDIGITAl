e502af7741ff79c54fc4972865f29715dc322c78
cocoxim <109473551+cocoxim@users.noreply.github.com>
2022-09-19 11:29:29 +0000

    live fixes

--- testdata/generics_package_alias/internal/api/api2.go
+++ testdata/generics_package_alias/internal/api/api2.go
@@ -19,4 +19,27 @@ func CreateMovie03() {
 // @Description Create a new movie production
 // @Accept  json
 // @Produce  json
-// @Success 200 {object} myv2.RenamedListR
+// @Success 200 {object} myv2.RenamedListResult[myv2.RenamedProductDto] ""
+// @Router /api04 [post]
+func CreateMovie04() {
+	_ = myv2.ListResult[myv2.ProductDto]{}
+}
+
+// @Summary Create movie
+// @Description Create a new movie production
+// @Accept  json
+// @Produce  json
+// @Success 200 {object} myv1.ListResult[myv2.ProductDto] ""
+// @Router /api05 [post]
+func CreateMovie05() {
+	_ = myv1.ListResult[myv2.ProductDto]{}
+}
+
+// @Summary Create movie
+// @Description Create a new movie production
+// @Accept  json
+// @Produce  json
+// @Success 200 {object} myv1.RenamedListResult[myv2.RenamedProductDto] ""
+// @Router /api06 [post]
+func CreateMovie06() {
+	_ = myv1.ListResult[myv2.Produ
