9d4ff1139818865024d2edceca5c017086418271
cocoxim <109473551+cocoxim@users.noreply.github.com>
2022-09-25 12:12:34 +0000

    Prepare 1.5.1 release

--- testdata/generics_package_alias/internal/api/api4.go
+++ testdata/generics_package_alias/internal/api/api4.go
@@ -2,4 +2,15 @@ package api
 
 import (
 	"github.com/swaggo/swag/testdata/generics_package_alias/external/external1"
-	_ "github.com/swaggo/swag/testdata/generics_package_al
+	_ "github.com/swaggo/swag/testdata/generics_package_alias/internal/path1/v1"
+)
+
+// @Summary Create movie
+// @Description models imported from an external package
+// @Accept  json
+// @Produce  json
+// @Success 200 {object} v1.ListResult[external1.Customer] ""
+// @Router /api11 [post]
+func CreateMovie11() {
+	var _ external1.Customer
+}
---
+++ testdata/generics_package_alias/internal/api/api5.go
@@ -0,0 +1,12 @@
+package api
+
+import (
+	myexternal "github.com/swaggo/swag/testdata/generics_package_alias/external/external2"
+	_ "github.com/swaggo/swag/testdata/generics_package_alias/internal/path1/v1"
+)
+
+// @Summary Create movie
+// @Description models imported from a named external package
+// @Accept  json
+// @Produce  json
+// @Success 200 {object} v1.Li
