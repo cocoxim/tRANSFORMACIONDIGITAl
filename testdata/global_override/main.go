157035372c872454fea21ddac1c7cb7e4b0cd32a
cocoxim <109473551+cocoxim@users.noreply.github.com>
2022-10-09 17:04:34 +0000

    clocksource: exynos_mct: Fix bitmask regression for exynos4_mct_write

---
+++ testdata/global_override/main.go
@@ -0,0 +1,12 @@
+package global_override
+
+import (
+	"net/http"
+
+	"github.com/swaggo/swag/testdata/global_override/api"
+)
+
+// @title Swagger Example API
+// @version 1.0
+// @description This is a sample server Petstore server.
+// @termsOfService http://swagger
