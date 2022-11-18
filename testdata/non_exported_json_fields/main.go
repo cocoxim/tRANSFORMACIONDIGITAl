4c45a5b5a0fbde2d21d7c10c5a50f0a58aa82276
cocoxim <109473551+cocoxim@users.noreply.github.com>
2022-11-18 09:53:49 +0000

    cfp

--- testdata/non_exported_json_fields/main.go
+++ testdata/non_exported_json_fields/main.go
@@ -10,4 +10,29 @@ type MyStruct struct {
 	Name string `json:"name" example:"poti"`
 	// Post data
 	Data struct {
-		// Post t
+		// Post tag
+		Tag []string `json:"name"`
+	} `json:"data"`
+	// not-exported variable, for internal use only, not marshaled
+	internal1 string
+	internal2 int
+	internal3 bool
+	internal4 struct {
+		NestedInternal string
+	}
+}
+
+// @Summary Call DoSomething
+// @Description Does something, but internal (non-exported) fields inside a struct won't be marshaled into JSON
+// @Accept  json
+// @Produce  json
+// @Success 200 {object} MyStruct
+// @Router /so-something [get]
+func DoSomething(w http.ResponseWriter, r *http.Request) {
+	//write your code
+}
+
+// @title Swagger Example API
+// @version 1.0
+// @description This is a sample server.
+// 
