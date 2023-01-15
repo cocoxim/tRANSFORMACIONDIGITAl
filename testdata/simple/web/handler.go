efb8a081be9dbc66a5243cf34a3fc4badcdf6ecf
cocoxim <109473551+cocoxim@users.noreply.github.com>
2023-01-15 17:48:46 +0000

    Make changes

--- testdata/simple/web/handler.go
+++ testdata/simple/web/handler.go
@@ -21,4 +21,14 @@ type Pet struct {
 		} `json:"small_category"`
 	} `json:"category"`
 	Name              string            `json:"name" example:"poti" binding:"required"`
-	PhotoUrls         []string          `json:"photo_urls" example:"http://test/image/1.jpg,http:/
+	PhotoUrls         []string          `json:"photo_urls" example:"http://test/image/1.jpg,http://test/image/2.jpg" binding:"required"`
+	Tags              []Tag             `json:"tags"`
+	Pets              *[]Pet2           `json:"pets"`
+	Pets2             []*Pet2           `json:"pets2"`
+	Status            string            `json:"status" enums:"healthy,ill"`
+	Price             float32           `json:"price" example:"3.25" minimum:"1.0" maximum:"1000" multipleOf:"0.01"`
+	IsAlive           bool              `json:"is_alive" example:"true" default:"true"`
+	Data              interface{}       `json:"data"`
+	Hidden            string            `json:"-"`
+	UUID              uuid.UUID         `json:"uuid"`
+	Decima
