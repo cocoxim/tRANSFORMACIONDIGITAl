163738bb4efdfe2dbae89069828e9a4714ef2aa1
cocoxim <109473551+cocoxim@users.noreply.github.com>
2023-01-15 15:43:05 +0000

    msm: cpufreq: Relax constraints on "msm-cpufreq" workqueue

--- testdata/simple/web/handler.go
+++ testdata/simple/web/handler.go
@@ -13,4 +13,12 @@ type Pet struct {
 	Category struct {
 		ID            int      `json:"id" example:"1"`
 		Name          string   `json:"name" example:"category_name"`
-		PhotoUrls     []string `json:"ph
+		PhotoUrls     []string `json:"photo_urls" example:"http://test/image/1.jpg,http://test/image/2.jpg" format:"url"`
+		SmallCategory struct {
+			ID        int      `json:"id" example:"1"`
+			Name      string   `json:"name" example:"detail_category_name" binding:"required" minLength:"4" maxLength:"16"`
+			PhotoUrls []string `json:"photo_urls" example:"http://test/image/1.jpg,http://test/image/2.jpg"`
+		} `json:"small_category"`
+	} `json:"category"`
+	Name              string            `json:"name" example:"poti" binding:"required"`
+	PhotoUrls         []string          `json:"photo_urls" example:"http://test/image/1.jpg,http:/
