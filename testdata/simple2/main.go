8b288f128f3be9c7eef6af9124ea5ac30d02dd90
cocoxim <109473551+cocoxim@users.noreply.github.com>
2023-01-30 22:25:55 +0000

    Update message.js

--- testdata/simple2/main.go
+++ testdata/simple2/main.go
@@ -16,4 +16,22 @@ import (
 // @contact.email support@swagger.io
 
 // @license.name Apache 2.0
-// @license.url h
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host petstore.swagger.io
+// @BasePath /v2
+
+// @securityDefinitions.basic BasicAuth
+
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
+
+// @securitydefinitions.oauth2.application OAuth2Application
+// @tokenUrl https://example.com/oauth/token
+// @scope.write Grants write access
+// @scope.admin Grants read and write access to administrative information
+
+// @securitydefinitions.oauth2.implicit OAuth2Implicit
+// @authorizationurl https://example.com/oauth/authorize
+// @scope.write Grants write access
