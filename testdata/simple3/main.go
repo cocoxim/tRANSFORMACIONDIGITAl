c33a33f69adf43bd125cc575f2c99408b033362b
cocoxim <109473551+cocoxim@users.noreply.github.com>
2023-02-09 20:21:57 +0000

    Commit automatically

--- testdata/simple3/main.go
+++ testdata/simple3/main.go
@@ -3,4 +3,29 @@ package main
 import (
 	"net/http"
 
-	"githu
+	"github.com/swaggo/swag/testdata/simple3/api"
+)
+
+// @title Swagger Example API
+// @version 1.0
+// @description This is a sample server Petstore server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host petstore.swagger.io
+// @BasePath /v2
+
+// @securityDefinitions.basic BasicAuth
+
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
+
+// @securitydefinitions.oauth2.application OAuth2Application
+// @tokenUrl https://
