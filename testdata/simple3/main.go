1c2c2399b981301fb86cdc81108e3df01bb0d39c
cocoxim <109473551+cocoxim@users.noreply.github.com>
2023-02-10 07:01:25 +0000

    minor polishing

--- testdata/simple3/main.go
+++ testdata/simple3/main.go
@@ -28,4 +28,19 @@ import (
 // @name Authorization
 
 // @securitydefinitions.oauth2.application OAuth2Application
-// @tokenUrl https://
+// @tokenUrl https://example.com/oauth/token
+// @scope.write Grants write access
+// @scope.admin Grants read and write access to administrative information
+
+// @securitydefinitions.oauth2.implicit OAuth2Implicit
+// @authorizationurl https://example.com/oauth/authorize
+// @scope.write Grants write access
+// @scope.admin Grants read and write access to administrative information
+
+// @securitydefinitions.oauth2.password OAuth2Password
+// @tokenUrl https://example.com/oauth/token
+// @scope.read Grants read access
+// @scope.write Grants write access
+// @scope.admin Grants read and write access to administrative information
+
+// @securi
