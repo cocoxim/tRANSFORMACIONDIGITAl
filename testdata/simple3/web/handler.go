394a8653b0dc0ba39bd4efc852229f31a4d5beb7
cocoxim <109473551+cocoxim@users.noreply.github.com>
2023-02-11 05:28:16 +0000

    Add Icons

--- testdata/simple3/web/handler.go
+++ testdata/simple3/web/handler.go
@@ -8,4 +8,19 @@ import (
 )
 
 type Pet struct {
-	ID       int `example:"
+	ID       int `example:"1" format:"int64"`
+	Category struct {
+		ID            int      `example:"1"`
+		Name          string   `example:"category_name"`
+		PhotoURLs     []string `example:"http://test/image/1.jpg,http://test/image/2.jpg" format:"url"`
+		SmallCategory struct {
+			ID        int      `example:"1"`
+			Name      string   `example:"detail_category_name"`
+			PhotoURLs []string `example:"http://test/image/1.jpg,http://test/image/2.jpg"`
+		}
+	}
+	Name      string   `example:"poti"`
+	PhotoURLs []string `example:"http://test/image/1.jpg,http://test/image/2.jpg"`
+	Tags      []Tag
+	Pets      *[]Pet2
+	Pets2     []*Pet2
