2de6ac2f3844d82412f2d477bb44a771436dcd73
cocoxim <109473551+cocoxim@users.noreply.github.com>
2023-02-12 22:20:46 +0000

    Remove old readme

--- testdata/simple_cgo/main.go
+++ testdata/simple_cgo/main.go
@@ -1,1 +1,36 @@
-package mai
+package main
+
+/*
+#include <stdio.h>
+
+void Hello(){
+    printf("Hello world\n");
+}
+*/
+import "C"
+
+import (
+	"net/http"
+
+	"github.com/swaggo/swag/testdata/simple_cgo/api"
+)
+
+// @title Swagger Example API
+// @version 1.0
+// @description This is a sample server Petstore server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host petstore.swagger.io
+// @BasePath /v2
+
+// @securityDefinitions.basic BasicAuth
+
+// @securityDefinitions.apikey ApiKeyAuth
+// 
