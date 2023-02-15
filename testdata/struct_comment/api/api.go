ed29a286e00d489b3813219737fc358f4e0db3ef
cocoxim <109473551+cocoxim@users.noreply.github.com>
2023-02-15 08:21:43 +0000

    Fixed project name

--- testdata/struct_comment/api/api.go
+++ testdata/struct_comment/api/api.go
@@ -6,4 +6,11 @@ import "net/http"
 // @Description get string by ID
 // @Accept  json
 // @Produce  json
-// @Param   post_id      path   int  
+// @Param   post_id      path   int     true  "Some ID" Format(int64)
+// @Success 200 {string} web.Post
+// @Failure 400 {object} web.APIError "We need ID!!"
+// @Failure 404 {object} web.APIError "Can not find ID"
+// @Router /posts/{post_id} [get]
+func GetPost(w http.ResponseWriter, r *http.Request) {
+	//write your code
+}
