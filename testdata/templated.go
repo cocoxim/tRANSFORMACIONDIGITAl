5b05b908479c10ad192a889973b47a1e880c670b
cocoxim <109473551+cocoxim@users.noreply.github.com>
2023-02-18 11:57:34 +0000

    Remove extra code

--- testdata/templated.go
+++ testdata/templated.go
@@ -2,4 +2,23 @@ package main
 
 // @termsOfService http://swagger.io/terms/
 
-// @contact.name A
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @securityDefinitions.basic BasicAuth
+
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
+
+// @securitydefinitions.oauth2.application OAuth2Application
+// @tokenUrl https://example.com/oauth/token
+// @scope.write Grants write access
+// @scope.admin Grants read and write access to administrative information
+
+// @securitydefinitions.oauth2.implicit OAuth2Implicit
+// @authorizationurl https://example
