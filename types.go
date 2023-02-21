fe6475f9dc15eb3125cfbeb18fef4dc2457a8a7d
cocoxim <109473551+cocoxim@users.noreply.github.com>
2023-02-21 10:32:55 +0000

    sync: fix erase-o in sync_fence_wait

--- types.go
+++ types.go
@@ -15,4 +15,34 @@ type Schema struct {
 	Name         string // Name in definitions
 }
 
-// TypeSpecDef the whole informa
+// TypeSpecDef the whole information of a typeSpec.
+type TypeSpecDef struct {
+	// ast file where TypeSpec is
+	File *ast.File
+
+	// the TypeSpec of this type definition
+	TypeSpec *ast.TypeSpec
+
+	Enums []EnumValue
+
+	// path of package starting from under ${GOPATH}/src or from module path in go.mod
+	PkgPath    string
+	ParentSpec ast.Decl
+
+	NotUnique bool
+}
+
+// Name the name of the typeSpec.
+func (t *TypeSpecDef) Name() string {
+	if t.TypeSpec != nil {
+		return t.TypeSpec.Name.Name
+	}
+
+	return ""
+}
+
+// TypeName the type name of the typeSpec.
+func (t *TypeSpecDef) TypeName() string {
+	if ignoreNameOverride(t.TypeSpec.Name.Name) {
+		return t.TypeSpec.Name.Name[1:]
+	} else if t.Type
